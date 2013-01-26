4ec40ae6ed84044d088de657bb20caa904947a3d
Oscar Suro <outercript@gmail.com>
2013-01-26 16:18:50 -0600

    float : moved structures union and enum to h file
    float : moved structures union and enum to h file
    
        moved structures from c to h file
    
    Signed-off-by: Oscar Suro <outercript@gmail.com>

--- flotante/main.c
+++ flotante/main.c
@@ -2,25 +2,6 @@
 #include <stdlib.h>
 #include "main.h"
 
-const uint8_t DEBUG=1;
-
-enum{
-    FALSE,
-    TRUE
-};
-
-typedef union {
-    uint8_t  byte[4];
-    uint16_t dbyte[2];
-    uint32_t lword;
-    float    fword;
-    struct {
-        unsigned int Mantissa: 23;
-        unsigned int Exponent: 8;
-        unsigned int Sign: 1;
-    } My;
-}Float32;
-
 void print_word_size(){
     printf("Char : %u\n", sizeof(char));
     printf("Short : %u\n", sizeof(short));
--- flotante/main.h
+++ flotante/main.h
@@ -2,5 +2,21 @@
 #define FLOAT_LIB
 
 // your declarations here
+enum{
+    FALSE,
+    TRUE
+};
+
+typedef union {
+    uint8_t  byte[4];
+    uint16_t dbyte[2];
+    uint32_t lword;
+    float    fword;
+    struct {
+        unsigned int Mantissa: 23;
+        unsigned int Exponent: 8;
+        unsigned int Sign: 1;
+    } My;
+}Float32;
 
 #endif
