e5a96c530301116b3d455c2077bc6776e1ebda0f
Emiliano Gibran Gonzalez Perez <haibrayn@hotmail.com>
2013-01-28 21:43:51 -0600

    Multiplication: Removed unused index
    Signed-off-by: Emiliano Gibran Gonzalez Perez <haibrayn@hotmail.com>

--- flotante/main.c
+++ flotante/main.c
@@ -104,7 +104,6 @@ Float32 float32_addition_substraction(Float32 x, Float32 y, uint8_t operation){
 /*********   Multiplication Algorithm    ************/
 
 Float32 float32_multiply(Float32 a, Float32 b){
-    uint8_t index;
     Float32 Am, Bm, Cr;
 
     Cr.lword = 0;
